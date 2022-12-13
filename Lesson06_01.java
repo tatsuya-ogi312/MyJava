9b0a1f85ec639def288c5329065ed52e80908215
tatsuya-ogi312 <112700622+tatsuya-ogi312@users.noreply.github.com>
2022-12-13 23:08:30 +0900

    lesson1.1

--- Lesson06_01.java
+++ Lesson06_01.java
@@ -1,8 +1,13 @@
 class Lesson06_01 {
   public static void main(String args[]){
-    int var;
-    var = 1;
+    int var = 1;
+    int var01 = 2;
+    int var_01 = 3;
+    int var$01 = 4;
 
     System.out.println(var);
+    System.out.println(var01);
+    System.out.println(var_01);
+    System.out.println(var$01);
   }
 }
